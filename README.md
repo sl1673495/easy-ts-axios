95e7db4afb0d95feefb641be4f562f311db7563b
sl1673495 <949589855@qq.com>
2019-04-25 22:36:39 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### ts-axios
+使用TypeScript实现axios
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+import { buildURL } from './helpers/url'
+
+function axios(config: AxiosRequestConfig): void {
+    processConfig(config)
+    xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+    config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+    const { url, params } = config
+    return buildURL(url, params)
+}
+
+export default axios
